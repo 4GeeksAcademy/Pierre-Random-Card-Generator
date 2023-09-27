c28618eb765cf545dccae96c4e5b17160c50a8a3
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-09-26 23:09:11 -0400

    finished random card generator error fixed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+import './style.css';
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import './assets/img/spades.svg';
+import './assets/img/hearts.svg';
+import './assets/img/diamond.svg';
+import './assets/img/spades.svg';
+
+const cards = ['./spades.svg', './hearts.svg', './diamond.svg', './spades.svg'];
+
+const cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+const cardNumber = document.querySelector('.card-number');
+const cardContainer = document.querySelector('.card-container');
+const topImage = document.querySelector('.top-img');
+const bottomImage = document.querySelector('.bottom-img');
+const changeCardButton = document.querySelector('.change-card');
+const widthInput = document.querySelector('#width');
+const heightInput = document.querySelector('#height');
+
+const generateRandomElement = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const generateRandomCards = () => {
+  topImage.src = generateRandomElement(cards);
+  bottomImage.src = topImage.src;
+  cardNumber.innerHTML = generateRandomElement(cardNumbers);
+};
+
+const changeWidth = (e) => {
+  cardContainer.style.width = `${e.target.value}px`;
+};
+
+const changeHeight = (e) => {
+  cardContainer.style.height = `${e.target.value}px`;
+};
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCards();
 };
+
+changeCardButton.addEventListener('click', generateRandomCards);
+
+widthInput.addEventListener('input', (e) => {
+  changeWidth(e);
+});
+
+heightInput.addEventListener('input', (e) => {
+  changeHeight(e);
+});
+
+setInterval(() => {
+  generateRandomCards();
+  console.log('10 secs passed');
+}, 10000);
